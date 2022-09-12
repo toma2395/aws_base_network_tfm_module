55cc05bcb7aa01d961d6b62ca81afac9d9c09a18
toma2395 <tomasz.duniec@rackspace.com>
2022-09-12 18:56:26 +0200

    fmt

--- example/main.tf
+++ example/main.tf
@@ -11,10 +11,10 @@ module "network" {
   # source = "git@github.com:toma2395/aws_base_network_tfm_module.git//?ref=v0.2.0"
   source = "../" # local path
 
-  environment        = "production"
-  cidr_range         = "172.16.0.0/16"
-  owner              = var.owner
-  project_name       = "MySampleProject"
-  create_nat_gateway = true
+  environment                      = "production"
+  cidr_range                       = "172.16.0.0/16"
+  owner                            = var.owner
+  project_name                     = "MySampleProject"
+  create_nat_gateway               = true
   multi_subnet_nat_gateway_for_vpc = false
 }
