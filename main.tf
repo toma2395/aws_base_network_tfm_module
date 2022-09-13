0bed822ef45c6178dcaf48ecc8e5984c1c786aae
toma2395 <tomasz.duniec@rackspace.com>
2022-09-13 16:41:58 +0200

    nat_eip

--- main.tf
+++ main.tf
@@ -97,7 +97,6 @@ resource "aws_route_table_association" "route_table_association" {
 resource "aws_nat_gateway" "many_private_nat_gateways" {
   for_each          = local.enable_nat_gateway && var.multi_subnet_nat_gateway_for_vpc ? aws_subnet.public_subnet : {}
   subnet_id         = each.value.id
-  connectivity_type = "private"
 
   depends_on = [
     aws_internet_gateway.my_igw
@@ -113,7 +112,6 @@ resource "aws_nat_gateway" "one_private_nat_gateway" {
   count             = local.enable_nat_gateway && !var.multi_subnet_nat_gateway_for_vpc ? 1 : 0
   allocation_id     = aws_eip.nat_eip[0].id
   subnet_id         = aws_subnet.public_subnet[local.public_cidr_ranges[local.default_private_subnet_index]].id
-  connectivity_type = "private"
 
   depends_on = [
     aws_internet_gateway.my_igw
