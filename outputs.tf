8609e1c07ae4ab9d08c23279903621d0faa0a37e
toma2395 <tomasz.duniec@rackspace.com>
2022-09-13 09:42:43 +0200

    converting readme

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,25 @@
 output "core_vpc_id" {
-  value = aws_vpc.core_vpc.id
+  value       = aws_vpc.core_vpc.id
+  description = "vpc id of created network"
 }
 
 
 output "private_subnets_ips" {
-  description = "The IPs for the private subnets"
+  description = "IPs for the private subnets"
   value       = local.private_cidr_ranges
 }
 
 output "private_subnets_id" {
-  description = "Identifiers fo private subnets"
+  description = "Identifiers of private subnets"
   value       = { for k, v in aws_subnet.private_subnet : k => v.id }
 }
 
 output "public_subnets_ips" {
-  description = "The IPs for the private subnets"
+  description = "IPs for the private subnets"
   value       = local.public_cidr_ranges
 }
 
 output "public_subnets_id" {
-  description = "Identifiers fo public subnets"
+  description = "Identifiers of public subnets"
   value       = { for k, v in aws_subnet.public_subnet : k => v.id }
 }
